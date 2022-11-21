5227ea1fc8bd12950d334f6a952b6525c5732b63
ByronEsteban <91495851+ByronEsteban@users.noreply.github.com>
2022-11-20 23:30:04 -0300

    Update Play.js

--- client/src/Play.js
+++ client/src/Play.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import Question from './Items';
 
 const Play = props => {
 
   const [questions, setQuestions] = useState([]);
   const [filteredQuestions, setFilteredQuestions] = useState([]);
-  const [selectedQuestion, setSelectedQuestion] = useState([]);
+  const [trueAnswer, setTrueAnswer] = useState([
+    {correct: false}, {correct: false}, {correct: false}, {correct: false}]);
+
+  const handleChange = e => {
+    const name = e.target.id;
+    console.log(name);
+    for (let i = 0; i < 4; i++)
+      trueAnswer[i].correct = false;
+    trueAnswer[name].correct = true;
+    console.log(trueAnswer);
+  }
 
   const handleClick = e => {
     const topic = e.target.previousSibling.value;
@@ -17,14 +26,40 @@ const Play = props => {
   }
 
   const sendAnswer = e => {
-    const topic = filteredQuestions
-    setFilteredQuestions(questions.filter(question =>
-      question.tema == topic
-    ));
-    console.log(filteredQuestions);
-    let r = Math.floor(Math.random()*filteredQuestions.length);
-    console.log(r);
+    let j = 0;
+    for (let i = 0; i < 4; i++) {
+      if (trueAnswer[i].correct == true && filteredQuestions[r].answers[i].correct == true) {
+        j = 1;
+      }
+    }
+    if (j) {
+      alert("Biennn!!! No sos tan bruto");
+      console.log("bieeennn");
+    }
+    else {
+      alert("Tremendo maloooo");
+      console.log("malll");
+    }
   }
+
+  const Question = props => {
+
+    const { text, tema, answers } = props.question;
+  
+    return (
+      <div name="Callate Amparo">
+        <h2>{text}</h2>
+        {answers.map((answer, idx) => (
+          <div key={idx}>
+            <label>{answer.text}</label>
+            <input type="radio" name={text} id={idx} onChange={handleChange}/>
+            <br />
+          </div>
+        ))}
+        <h4>Tema: {tema}</h4>
+      </div>
+    );
+  };
 
   useEffect(() => {
     axios.get('/question/preguntas')
@@ -33,6 +68,8 @@ const Play = props => {
        setQuestions(res.data);
      });
   },[]);
+
+  const r = Math.floor(Math.random()*filteredQuestions.length);
 
   return (
     <div>
@@ -49,9 +86,12 @@ const Play = props => {
       <input type="button" value="Jugar" onClick={handleClick}/>
       <div>
         {filteredQuestions.map((question,idx) => {
-          if (Math.floor(Math.random()*filteredQuestions.length) === idx){
+          if (r === idx){
             console.log(idx);
-            return (<Question key={idx} question={question} />)
+            return (<div>
+              <Question key={idx} question={question} />
+              <input type="button" value="Enviar" onClick={sendAnswer}/>
+              </div>)
           }
         }
         )}
