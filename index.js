ffcc1725596d49c4de28f82bd7b654b422bcadab
federico heredia <federicomartinheredia2006@gmail.com>
2022-11-22 12:47:27 -0300

    updatdsa

--- index.js
+++ index.js
@@ -17,16 +17,6 @@ mongoose.connect(DB)
 app.use(morgan('dev'));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 import questionRouter from './Routes/Router.js';
 app.use('/api', questionRouter);
