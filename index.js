bd8fa946a68705fc7cbb038495c1a037d51d0b8b
federico heredia <federicomartinheredia2006@gmail.com>
2022-11-22 12:40:16 -0300

    updater

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const DB = process.env.DB || 'mongodb+srv://preguntados:preguntados@preguntados.
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+// Exprees will serve up production assets
+app.use(express.static('client/build'));
+
+// Express serve up index.html file if it doesn't recognize route
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
+
 mongoose.connect(DB)
           .then(() => console.log("Mongo ok"))
           .catch(err => console.log(err));
